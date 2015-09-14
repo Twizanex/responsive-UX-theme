6e88272d7774a2fa92179bce6c2ee15e1c515b6c
Stas <melnik909@ya.ru>
2015-09-14 19:56:15 +0300

    init version 2

---
+++ core/js/app.js
@@ -0,0 +1,14 @@
+/*
+author: Developed by Stas Melnikov. http://stas-melnikov.ru
+e-mail: melnik909@ya.ru
+linkedIn: https://www.linkedin.com/in/melnik909
+github: https://github.com/melnik909
+facebook: https://www.facebook.com/melnik909
+Template created with use of a Responsive Framework http://stas-melnikov.ru/responsive_elements/
+*/
+
+//@prepros-append jquery.js
+
+//@prepros-append init.js 
+//@prepros-append main.js 
+ 
